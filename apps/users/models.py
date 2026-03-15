e5e0771c7d7a558c6f1d42724a9b92ef7b86eadd
syednhussain5 <syednoorulhussain23@gmail.com>
2026-03-15 22:39:02 +0530

    Fix Pillow and avatar field

--- apps/users/models.py
+++ apps/users/models.py
@@ -14,7 +14,7 @@ class User(AbstractUser):
 
     email = models.EmailField(unique=True)
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='student')
-    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+    avatar = models.CharField(max_length=500, null=True, blank=True)
     bio = models.TextField(blank=True)
 
     # Gamification
